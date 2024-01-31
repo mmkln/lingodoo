ca6b89cf61f43bb3bb80f5f4abcc7d3a276d7cd4
Michael Mikulin <90753237+mmkln@users.noreply.github.com>
2024-01-31 22:29:04 +0100

    Add SelectLanguage component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export {default as StarsRating} from './StarsRating';
 export * from './ProgressBar';
 export * from './ReviewWord';
 export * from './LearnNewWord';
+export * from './SelectLanguage';
---
+++ src/constants/appLanguages.ts
@@ -0,0 +1,12 @@
+import {AppLanguage, LanguageCode} from "../models";
+
+export const APP_LANGUAGES: AppLanguage[] = [
+    {
+        name: 'English',
+        code: LanguageCode.EN,
+    },
+    {
+        name: 'Magyar',
+        code: LanguageCode.HU,
+    },
+];
